e26c384de2441ee3f8143a6a96d24f5286140561
Matthias Wahl <matthiaswahl@m7w3.de>
2016-01-08 23:51:49 +0100

    change user table and create proposals table

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,18 +1,18 @@
 -- name: create-user!
 -- creates a new user record
 INSERT INTO users
-(id, first_name, last_name, email, pass)
-VALUES (:id, :first_name, :last_name, :email, :pass)
+(id, nickname, email)
+VALUES (:id, :nickname, :email)
 
 -- name: update-user!
 -- update an existing user record
 UPDATE users
-SET first_name = :first_name, last_name = :last_name, email = :email
+SET nickname = :nickname, email = :email
 WHERE id = :id
 
 -- name: get-user
 -- retrieve a user given the id.
-SELECT * FROM users
+SELECT id, nickname, email FROM users
 WHERE id = :id
 
 -- name: delete-user!
